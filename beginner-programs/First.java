0423e096a45fd7f36eb639f101b6a199e30e8dff
SPRandhawa <jaibirsim@gmail.com>
2025-11-09 11:13:40 +0530

    Added first Java program - Hello World

---
+++ beginner-programs/First.java
@@ -0,0 +1,10 @@
+// Simple Java program to display "Hello, World!" on the screen
+// This is often the first program beginners write in any language
+
+class HelloWorld
+  {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+}
